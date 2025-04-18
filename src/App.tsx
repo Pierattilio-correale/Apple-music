f0a376d05a9ea4fe5a860fe6b2824f482aca6030
Pierattilio-correale <pierattiliocorreale@yahoo.it>
2025-04-18 09:23:00 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+function App() {
+  return <></>;
+}
+
+export default App;
